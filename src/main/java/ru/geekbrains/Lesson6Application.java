43d0faa34a3380401d348d1fb949ffbf76314d3a
listpv <listpv@mail.ru>
2021-03-23 08:54:36 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/Lesson6Application.java
@@ -0,0 +1,12 @@
+package ru.geekbrains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson6Application.class, args);
+    }
+}
